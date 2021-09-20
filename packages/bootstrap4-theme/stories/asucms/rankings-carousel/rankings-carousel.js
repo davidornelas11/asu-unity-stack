648efa1d4359bde0c912f471b24d2fbeee5368b1
nicolasBaudon <nicobaudon01@gmail.com>
2021-09-20 11:49:59 -0300

    feat(bootstrap4-theme): rankings carousel dataLayer implementation

--- packages/bootstrap4-theme/stories/asucms/rankings-carousel/rankings-carousel.js
+++ packages/bootstrap4-theme/stories/asucms/rankings-carousel/rankings-carousel.js
@@ -1,65 +1,90 @@
 import Glide from '../image-carousel/glide-3-4-1';
 
-
 export const initCarousel = () => {
-  var ArrowDisabler = function (Glide, Components, Events) {
+  const pushGAEvent = (args) => {
+    const { dataLayer } = window;
+    const event = {
+      action: 'click',
+      name: 'onclick',
+      region: 'main content',
+      section: '#1 in the us for innovation',
+      ...args,
+    };
+    if (dataLayer) dataLayer.push(event);
+  };
+
+  const arrowDisabler = (Glide, Components, Events) => {
     return {
       mount() {
         // Only in effect when rewinding is disabled
         if (Glide.settings.rewind) {
-          return
+          return;
         }
 
         Glide.on(['mount.after', 'run'], () => {
           // Filter out arrows_control
           for (let controlItem of Components.Controls.items) {
             if (controlItem.className !== 'glide__arrows') {
-              continue
+              continue;
             }
 
             // Set left arrow state
-            var left = controlItem.querySelector('.glide__arrow--left')
+            var left = controlItem.querySelector('.glide__arrow--left');
             if (left) {
               if (Glide.index === 0) {
-                left.classList.add("glide__arrow--disabled");
+                left.classList.add('glide__arrow--disabled');
                 //left.setAttribute('disabled', '') // Disable on first slide
               } else {
-                left.classList.remove("glide__arrow--disabled");
+                left.classList.remove('glide__arrow--disabled');
                 //left.removeAttribute('disabled') // Enable on other slides
               }
             }
 
             // Set right arrow state
-            var right = controlItem.querySelector('.glide__arrow--right')
+            var right = controlItem.querySelector('.glide__arrow--right');
             if (right) {
-                if (Glide.index === Components.Sizes.length - Glide.settings.perView) {
-                  right.classList.add("glide__arrow--disabled");
-                  //right.setAttribute('disabled', '') // Disable on last slide
-                } else {
-                  right.classList.remove("glide__arrow--disabled");
-                  //right.removeAttribute('disabled') // Disable on other slides
-                }
+              if (
+                Glide.index ===
+                Components.Sizes.length - Glide.settings.perView
+              ) {
+                right.classList.add('glide__arrow--disabled');
+                //right.setAttribute('disabled', '') // Disable on last slide
+              } else {
+                right.classList.remove('glide__arrow--disabled');
+                //right.removeAttribute('disabled') // Disable on other slides
+              }
             }
           }
-        })
-      }
-    }
-  }
+        });
+      },
+    };
+  };
 
+  const elements = document.querySelectorAll('[data-ga]');
+  elements.forEach((element) =>
+    element.addEventListener('click', () => {
+      const args = {
+        event: element.getAttribute('data-ga-event').toLowerCase(),
+        type: element.getAttribute('data-ga-type').toLowerCase(),
+        text: element.getAttribute('data-ga').toLowerCase(),
+      };
+      pushGAEvent(args);
+    })
+  );
 
   new Glide('#rankings-carousel', {
-      type: "slider", // No wrap-around.
-      focusAt: 0,
-      bound: true, // Only if type slider with focusAt 0
-      rewind: false, // Only if type slider
-      keyboard: true, // Left/Right arrow key support for slides - true is default. Accessible?
-      startAt: 0,
-      swipeThreshold: 80, // Distance required for swipe to change slide.
-      dragThreshold: 120, // Distance for mouse drag to change slide.
-      perTouch: 1, // Number of slides that can be moved per each swipe/drag.
-      peek: 0,
-      gap: 0,
-      perView: 1,
-      autoplay: 5000, //Change this to change time between scroll. Time is in miliseconds(5 seconds on this example)
-  }).mount({ ArrowDisabler });
-}
+    type: 'slider', // No wrap-around.
+    focusAt: 0,
+    bound: true, // Only if type slider with focusAt 0
+    rewind: false, // Only if type slider
+    keyboard: true, // Left/Right arrow key support for slides - true is default. Accessible?
+    startAt: 0,
+    swipeThreshold: 80, // Distance required for swipe to change slide.
+    dragThreshold: 120, // Distance for mouse drag to change slide.
+    perTouch: 1, // Number of slides that can be moved per each swipe/drag.
+    peek: 0,
+    gap: 0,
+    perView: 1,
+    autoplay: 5000, //Change this to change time between scroll. Time is in miliseconds(5 seconds on this example)
+  }).mount({ arrowDisabler });
+};
--- packages/bootstrap4-theme/stories/asucms/rankings-carousel/rankings-carousel.stories.js
+++ packages/bootstrap4-theme/stories/asucms/rankings-carousel/rankings-carousel.stories.js
@@ -4,7 +4,6 @@ import { initCarousel } from './rankings-carousel';
 
 export default createComponent('Rankings Carousel', 'ASUCMS');
 
-
 export const rankingsCarousel = createStory(
   <div class="image-carousel">
     <div id="rankings-carousel" class="glide rankings-carousel">
@@ -14,42 +13,53 @@ export const rankingsCarousel = createStory(
             <div class="asucms-image">
               <img
                 src="https://ux-annual-report.ws.asu.edu/sites/all/libraries/Prototype/images/210211-ASURankings-1Innovation%20(1).png"
-                alt="Replace with appropriate alt text" />
+                alt="Replace with appropriate alt text"
+              />
             </div>
           </li>
           <li class="glide__slide">
             <div class="asucms-image">
               <img
                 src="https://ux-annual-report.ws.asu.edu/sites/all/libraries/Prototype/images/210211-ASURankings-_BestBachelors.png"
-                alt="Replace with appropriate alt text" />
+                alt="Replace with appropriate alt text"
+              />
             </div>
           </li>
           <li class="glide__slide">
             <div class="asucms-image">
               <img
                 src="https://ux-annual-report.ws.asu.edu/sites/all/libraries/Prototype/images/210211-ASURankings-_Excellence.png"
-                alt="Replace with appropriate alt text" />
+                alt="Replace with appropriate alt text"
+              />
             </div>
           </li>
           <li class="glide__slide">
             <div class="asucms-image">
               <img
                 src="https://ux-annual-report.ws.asu.edu/sites/all/libraries/Prototype/images/210211-ASURankings-_Top10International.png"
-                alt="Replace with appropriate alt text" />
+                alt="Replace with appropriate alt text"
+              />
             </div>
           </li>
           <li class="glide__slide">
             <div class="asucms-image">
               <img
                 src="https://ux-annual-report.ws.asu.edu/sites/all/libraries/Prototype/images/210211-ASURankings-_Top25MBA.png"
-                alt="Replace with appropriate alt text" />
+                alt="Replace with appropriate alt text"
+              />
             </div>
           </li>
         </ul>
       </div>
 
       <div class="glide__arrows" data-glide-el="controls">
-        <button class="glide__arrow glide__arrow--left" data-glide-dir="<">
+        <button
+          class="glide__arrow glide__arrow--left"
+          data-glide-dir="<"
+          data-ga-event="select"
+          data-ga-type="carousel"
+          data-ga="left chevron"
+        >
           <svg
             aria-hidden="true"
             focusable="false"
@@ -60,10 +70,21 @@ export const rankingsCarousel = createStory(
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 100 100"
           >
-            <path fill="currentColor" d="M 50,0 L 60,10 L 20,50 L 60,90 L 50,100 L 0,50 Z" class="arrow" transform="translate(15,0)"></path>
+            <path
+              fill="currentColor"
+              d="M 50,0 L 60,10 L 20,50 L 60,90 L 50,100 L 0,50 Z"
+              class="arrow"
+              transform="translate(15,0)"
+            ></path>
           </svg>
         </button>
-        <button class="glide__arrow glide__arrow--right" data-glide-dir=">">
+        <button
+          class="glide__arrow glide__arrow--right"
+          data-glide-dir=">"
+          data-ga-event="select"
+          data-ga-type="carousel"
+          data-ga="right chevron"
+        >
           <svg
             aria-hidden="true"
             focusable="false"
@@ -74,18 +95,26 @@ export const rankingsCarousel = createStory(
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 100 100"
           >
-            <path fill="currentColor" d="M 50,0 L 60,10 L 20,50 L 60,90 L 50,100 L 0,50 Z" class="arrow" transform="translate(85,100) rotate(180)"></path>
+            <path
+              fill="currentColor"
+              d="M 50,0 L 60,10 L 20,50 L 60,90 L 50,100 L 0,50 Z"
+              class="arrow"
+              transform="translate(85,100) rotate(180)"
+            ></path>
           </svg>
         </button>
       </div>
       <div class="link-section">
-        <a href="example.com">View all rankings</a>
+        <a
+          href="example.com"
+          data-ga-event="link"
+          data-ga-type="internal link"
+          data-ga="view all rankings"
+        >
+          View all rankings
+        </a>
       </div>
     </div>
   </div>,
   initCarousel
-)
-
-
-
-
+);
